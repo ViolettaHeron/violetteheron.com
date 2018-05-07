57a2fa9098065abe746df710f92de521524eef94
Violette Heron <heron.violette@gmail.com>
2018-05-07 23:33:49 +0200

    Initial homepage : react create app with some differences

---
+++ src/logo/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Ursa from './ursa-major.svg'
+import "./logo.css"
+
+class Logo extends React.Component{
+    render = () => (
+        <div>
+            <img src={Ursa} className="header-logo" alt="logo" />
+
+        </div>
+    )
+
+    /*
+    * <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+    * <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    * */
+}
+
+export default Logo;
