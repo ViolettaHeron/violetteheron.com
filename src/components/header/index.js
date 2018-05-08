6f928135e53ad295a9a4be854cd2cf47b8fe1371
Violette Heron <heron.violette@gmail.com>
2018-05-08 10:44:36 +0200

    header + responsive of menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,27 @@
 import React  from 'react';
 import Logo   from '../../components/logo';
 import {Link} from 'react-router-dom';
-import './header.css'
+import './header.css';
 
 class Header extends React.Component {
 
     render() {
         return (
-            <header className="general-header">
-                <Logo/>
-                <nav className="header-menu">
-                    <Link to="/">Home</Link>
-                    <Link to="/about-us">About</Link>
-                </nav>
+            <header className="general-header--container">
+                <div className="general-header">
+                    <div className="general-header--title-container">
+                        <div className="general-header--title--logo">
+                            <Logo/>
+                        </div>
+                        <h1 className="general-header--title">Violette Héron</h1>
+                    </div>
+                    <nav className="general-header--menu">
+                        <Link to="/">Home</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/CV">CV</Link>
+                        <a href="https://github.com/VioletteHeron/" target="_blank">Github</a>
+                    </nav>
+                </div>
             </header>
         );
     }
