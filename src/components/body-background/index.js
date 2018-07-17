a4cdd5125686a931ba41083156bfde275e4d7064
Violette <v.heron@qwant.com>
2018-07-17 21:02:05 +0200

    clouds defiling as well

--- src/components/body-background/index.js
+++ src/components/body-background/index.js
@@ -3,9 +3,12 @@ import './body-background.css';
 
 const Background = props => (
     <div className="background--container">
-        <div className="background-mountain background--mountain-1"></div>
-        <div className="background-mountain background--mountain-2"></div>
-        <div className="background-mountain background--mountain-3"></div>
+        <div className="background-mountain background--mountain-1" />
+        <div className="background-mountain background--mountain-2" />
+        <div className="background-mountain background--mountain-3" />
+
+        <div className="background-cloud background--cloud-1" />
+        <div className="background-cloud background--cloud-2" />
     </div>
 );
 
